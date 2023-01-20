c5575e30497c72e899b92d0b91a5bdfb2c3bd06b
Роман Давыдов <davidos.corp.gmaol.com>
2023-01-20 16:01:01 +0300

    горячие клавиши для меню

--- LanClient/main.cpp
+++ LanClient/main.cpp
@@ -11,17 +11,21 @@ int main(int argc, char *argv[])
     QMenuBar mnuBar(&w);
     QMenu*   pmnu   = new QMenu("&Tools");
 
-    pmnu->addAction("&Wave Viewert", &w,
-                    SLOT(slot_show()), Qt::CTRL + Qt::Key_Q );
+    pmnu->addAction("&Wave Viewert",
+                    &w,
+                    SLOT(slot_show()),
+                    Qt::CTRL + Qt::Key_W );
 
     pmnu->addAction("&About Qt",
                     &a,
                     SLOT(aboutQt()),
-                    Qt::CTRL + Qt::Key_Q
-                   );
+                    Qt::CTRL + Qt::Key_A );
 
     pmnu->addSeparator();
-    pmnu->addAction("&Exit", &a, SLOT(quit()));
+    pmnu->addAction("&Exit",
+                    &a,
+                    SLOT(quit()),
+                    Qt::CTRL + Qt::Key_Q);
 
     mnuBar.addMenu(pmnu);
     mnuBar.show();
